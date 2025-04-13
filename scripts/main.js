f401c0c7f36f38ec91786d6d89bf8b02236b234e
Yassin El Fraygui <yassin.el_fraygui@smail.th-koeln.de>
2025-04-13 21:34:30 +0200

    Slideshow

---
+++ scripts/main.js
@@ -0,0 +1,23 @@
+document.addEventListener("DOMContentLoaded", () => {
+    const welcomeSection = document.querySelector(".welcome-section");
+    if(welcomeSection) console.log("Welcome section found!");
+
+    // Funktion zum durchgehenden Ändern der Bilder
+
+    function changeImages() {
+        setInterval(() => {
+            const images = [
+                './img/group-welcome-unsplash.jpg',
+                './img/lost-man-welcome-unsplash.jpg',
+                './img/mountain-welcome-unsplash.jpg'
+            ];
+            const randomIndex = Math.floor(Math.random() * images.length);
+            welcomeSection.style.backgroundImage = `url('${images[randomIndex]}')`;
+            welcomeSection.style.backgroundSize = "cover";
+            welcomeSection.style.backgroundPosition = "center";
+            welcomeSection.style.transition = "all 2s ease";
+        }, 7000); // Alle 5 Sekunden
+    }
+
+    changeImages();
+});
