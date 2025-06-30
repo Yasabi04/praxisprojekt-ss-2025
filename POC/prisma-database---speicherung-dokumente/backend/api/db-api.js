262f8bd21f68856fd3092b152cd98ab318934a8e
Yassin El Fraygui <yassin.el_fraygui@smail.th-koeln.de>
2025-06-30 16:29:15 +0200

    neue ordnerstruktur

--- POC/prisma-database---speicherung-dokumente/backend/api/db-api.js
+++ POC/prisma-database---speicherung-dokumente/backend/api/db-api.js
@@ -3,7 +3,8 @@ const { PrismaClient } = require('@prisma/client');
 const router = express.Router();
 const prisma = new PrismaClient();
 
-// GET /api/users - Alle Benutzer abrufen
+
+// localhost:3012/api/...
 router.get('/allusers', async (req, res) => {
     try{
         const users = await prisma.user.findMany();
@@ -11,6 +12,27 @@ router.get('/allusers', async (req, res) => {
     }
     catch (error){
         res.status(500).json({ error: 'Fehler beim Abrufen der Benutzer' });
+    }
+})
+
+router.get('/safedDocs/:id', async (req, res) => {
+    try{
+        const reqId = req.params.id
+        const docs = await prisma.document.findMany({
+            where: {
+                authorId: reqId
+            }
+        })
+        if(docs.length > 0){
+            res.json(docs)
+        }
+        else if(docs.length == 0) {
+            res.json({ message: 'Keine Dokumente gespeichert' })
+        }
+    }
+    catch (error){
+        console.log(error + 'B A D')
+        res.status(500).json({ error: 'Fehler beim Abrufen'})
     }
 })
 
---
+++ POC/prisma-database---speicherung-dokumente/backend/prisma/migrations/20250628214003_add_pdf_file_byte/migration.sql
@@ -0,0 +1,2 @@
+-- AlterTable
+ALTER TABLE "Document" ADD COLUMN     "pdfFile" BYTEA;
